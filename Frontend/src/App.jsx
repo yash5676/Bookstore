5ccc4237bd8ca84d2de32b10f5981e4f59d04f19
yash5676 <yashkumargoyal1804@gmail.com>
2024-04-24 11:38:20 +0530

    make navbar and banner

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Banner from './components/Banner'
 
 function App() {
   return(
   <>
   <Navbar/>
+  <Banner/>
   </>
   )
   }

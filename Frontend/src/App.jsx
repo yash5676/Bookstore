33500b610d11701c352c56c988d9f8ac5116aaca
yash5676 <yashkumargoyal1804@gmail.com>
2024-04-28 00:49:36 +0530

    made card page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,12 +3,15 @@ import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import Footer from './components/Footer'
 import Freebook from './components/Freebook'
+
 function App() {
   return(
   <>
   <Navbar/>
   <Banner/>
+  
   <Freebook/>
+ 
   <Footer/>
   
   </>
--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick-theme.css"
 import Slider from "react-slick"
 
 import list from "../../public/list.json"
+import Cards from './Cards'
 
 function Freebook() {
  const filterData=list.filter((data)=>data.category==="Free");
@@ -13,8 +14,8 @@ function Freebook() {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     initialSlide: 0,
     responsive: [
       {
@@ -31,7 +32,7 @@ function Freebook() {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          initialSlide: 2
+          initialSlide: 3
         }
       },
       {
@@ -47,38 +48,21 @@ function Freebook() {
   return (
     <>
     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
-      <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
+    <div>
+    <h1 className="font-semibold text-xl pb-2">Free Offered Courses</h1>
       <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
               et totam. Tempora amet atque expedita, quae corrupti totam sed
               pariatur corporis at veniam est voluptas animi!</p>
     </div>
+    
     <div>
     <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
+        
+        {filterData.map((item)=>(
+          <Cards item={item} key={item.id}/>
+        ))}
       </Slider>
+    </div>
     </div>
     </>
     
